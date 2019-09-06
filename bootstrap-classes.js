18b2eeac67a0c2c913c4d36a3023c6076a3c71e1
Mukul Khanna <mukulkhanna.96@gmail.com>
2019-09-06 23:30:31 +0530

    Initial Commit - JS Bootstrap3 Reference Helper

---
+++ bootstrap-classes.js
@@ -0,0 +1,62 @@
+class Bootstrap3 {
+    static Container = "container";
+    static ContainerFluid = "container-fluid";
+}
+
+Bootstrap3.Grid = class {
+    static Row = "row";
+    
+}
+
+Bootstrap3.Grid.Columns = class {
+    
+}
+
+Bootstrap3.Grid.Columns.ExtraSmall = class {
+    static XS1 = "col-xs-1";
+    static XS2 = "col-xs-2";
+    static XS3 = "col-xs-3";
+    static XS4 = "col-xs-4";
+    static XS5 = "col-xs-5";
+    static XS6 = "col-xs-6";
+    static XS7 = "col-xs-7";
+    static XS8 = "col-xs-8";
+    static XS9 = "col-xs-9";
+    static XS10 = "col-xs-10";
+    static XS11 = "col-xs-11";
+    static XS12 = "col-xs-12";
+}
+
+Bootstrap3.Grid.Columns.Small = class {
+    static SM1 = "col-sm-1";
+    static SM2 = "col-sm-2";
+    static SM3 = "col-sm-3";
+    static SM4 = "col-sm-4";
+    static SM5 = "col-sm-5";
+    static SM6 = "col-sm-6";
+    static SM7 = "col-sm-7";
+    static SM8 = "col-sm-8";
+    static SM9 = "col-sm-9";
+    static SM10 = "col-sm-10";
+    static SM11 = "col-sm-11";
+    static SM12 = "col-sm-12";
+}
+
+Bootstrap3.Grid.Columns.Medium = class {
+    static MD1 = "col-md-1";
+    static MD2 = "col-md-2";
+    static MD3 = "col-md-3";
+    static MD4 = "col-md-4";
+    static MD5 = "col-md-5";
+    static MD6 = "col-md-6";
+    static MD7 = "col-md-7";
+    static MD8 = "col-md-8";
+    static MD9 = "col-md-9";
+    static MD10 = "col-md-10";
+    static MD11 = "col-md-11";
+    static MD12 = "col-md-12";
+}
+
+Bootstrap3.Form = class {
+    static FormControl = "form-control";
+}
